716ef2c0162a0e4aa3c6a1f6b778f7472c0d0082
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2019-11-11 13:17:57 +0530

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+
+  myForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      mobile: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(10)])],
+    });
+  }
+
+  submitForm(formValues){
+    console.log(formValues["mobile"]);
+    console.log(formValues["password"]);
+  } 
 
 }
