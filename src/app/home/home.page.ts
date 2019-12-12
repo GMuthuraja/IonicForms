ae40fb5507529fa0cfdf67df7a70a871ff028a89
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2019-12-12 15:05:20 +0530

    changes made

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +9,27 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 export class HomePage implements OnInit {
 
-  myForm: FormGroup;
+  myFormGroup: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.myForm = this.formBuilder.group({
-      mobile: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(10)])],
-    });
+    // this.myFormGroup = this.formBuilder.group({
+    //   mobile: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*')])],
+    //   password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(10)])],
+    // });
+
+    this.myFormGroup = new FormGroup({
+      mobile: new FormControl('9025720760', Validators.required),
+      password: new FormControl('123', Validators.compose([Validators.required])),
+    })
+
   }
 
-  submitForm(formValues){
+  submitForm(formValues) {
+    console.log(this.myFormGroup.controls["mobile"].value);
     console.log(formValues["mobile"]);
     console.log(formValues["password"]);
-  } 
+  }
 
 }
